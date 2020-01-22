6d2285088e62b0149447f8f7cebb573eaf67780f
Aliaksiej Homza <aliaksiej@yomobile.com>
2020-01-22 17:50:36 +0300

    Add workbox as dependency, precache index html

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,11 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+import 'workbox-sw/build/workbox-sw'
+
+console.log(self.workbox)
+
+self.workbox.precaching.precacheAndRoute([
+  'index.html'
+])
 
 self.workbox.routing.registerRoute(
   /\.*/,
